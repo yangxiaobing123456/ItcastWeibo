b1ecc628a8e1645e8645c0afd3ef8c1644e573c7
MrwhYzZ <yuanzheng@yuanzhengdeMacBook-Pro.local>
2014-11-04 15:57:25 +0800

    Initial Commit

---
+++ ItcastWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ItcastWeibo
+//
+//  Created by yz on 14/11/4.
+//  Copyright (c) 2014年 iThinker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
